bfc23fb404da04a185e80c5b857e60c29ac4153a
root <root@app.local>
2015-01-12 14:24:34 +0000

    Modified angular test html

--- angular/js/base.js
+++ angular/js/base.js
@@ -2,6 +2,15 @@
 /**
  * base controller
  */
-NetCommonsApp.controller('NetCommons.base', function($scope) {
+NetCommonsApp.controller('NetCommons.base', function($scope, NcModuleConstants) {
 	$scope.content = 'Hello World!!';
+  $scope.value1 = NcModuleConstants.value1;
+  $scope.value2 = NcModuleConstants.value2;
+});
+
+
+NetCommonsApp.controller('Controller1', function($scope, NcModuleValues) {
+	$scope.content = 'Hello World 22222!!';
+  $scope.value1 = NcModuleValues.value1;
+  $scope.value2 = NcModuleValues.value2;
 });
--- angular/js/nc_module.js
+++ angular/js/nc_module.js
@@ -1,3 +1,21 @@
 
 var NcModule = angular.module('nc.module', []);
 
+NcModule.config(function(NcModuleConstants) {
+  console.log('NcModuleConstants');
+  console.log(NcModuleConstants);
+});
+
+//NcModule.config(function(NcModuleValues) {
+//  console.log(NcModuleValues);
+//});
+
+NcModule.constant('NcModuleConstants', {
+    value1: 'test constant 1',
+    value2: 'test constant 2'
+  });
+
+NcModule.value('NcModuleValues', {
+    value1: 'test value 1',
+    value2: 'test value 2'
+  });
